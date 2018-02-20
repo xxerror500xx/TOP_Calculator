98d18e5a3995828231e388a62ee8857c94234555
xxerror500xx <cliff.minks@gmail.com>
2018-02-19 22:01:11 -0600

    Implement Addition and subtraction functionality

--- js/master.js
+++ js/master.js
@@ -2,6 +2,7 @@ var calc = {
   init: function() {
     power = false;
     equation = [];
+    prevEquations = [];
     buttons = [
       {id:'a-clr', class:'btn-danger'},
       {id:'clr', class:'btn-danger'},
@@ -30,10 +31,11 @@ var calc = {
     console.log("in pwr()");
     console.log(power);
     if(power) {
-      $('#on-off').removeClass(' btn sketch-btn btn-danger');
+      $('#on-off').removeClass('btn sketch-btn btn-danger');
       $('#on-off').addClass('sketch-btn-outline');
       this.rmvBtnClasses();
       $('#on-off-txt').text('ON');
+      this.clrAll();
     } else {
       // remove sketch-btn-outline class
       $('#on-off-txt').text('OFF');
@@ -64,8 +66,6 @@ var calc = {
       console.log("adding val to equation: " + value);
       equation.push(value)
       this.updateCalcDisplay();
-    } else {
-      console.log("We needs dah POW-AH!");
     }
   },
   updateCalcDisplay: function() {
@@ -75,28 +75,69 @@ var calc = {
       $('#calc-disp').val('0');
     }
   },
-  clrCurrentEquation: function() {
-    $('#calc-disp').val('0');
-    equation = [];
-  },
   updatePaperTape: function(equationString, result) {
     // Row should equal pastEquations index
-    $('#p-tBody').append('<tr class=""><td class="tape-font">' + equationString + '</td><td class="tape-font">=' + result + '</td></tr>')
+    $('#p-tBody').append('<tr class=""><td class="tape-font">' + equationString + '</td><td class="tape-font"> = ' + result + '</td></tr>')
+  },
+  clrCurrentEquation: function() {
+    if (power) {
+      $('#calc-disp').val('0');
+      equation = [];
+    }
+  },
+  clrAll: function() {
+    if (power) {
+      this.clrCurrentEquation();
+      $('#p-tBody').empty();
+    }
+  },
+  del: function() {
+    if (equation.length > 0 && power) {
+      $('#calc-disp').val($('#calc-disp').val().slice(0, -1));
+      equation.pop();
+    }
+  },
+  equals: function() {
+    eqString = equation.join('');
+    console.log(eqString);
+    if(eqString.includes('*')) {
+      console.log("lets multiply it " + eqString);
+    }else if (eqString.includes('/')) {
+      console.log("lets divide it " + eqString);
+    }else if (eqString.includes('+')) {
+      variables = eqString.split('+');
+      result = 0;
+      for (var i = 0; i < variables.length; i++) {
+        result = eval(result + parseInt(variables[i]))
+      }
+      console.log(eqString + ' = ' + result);
+    }else if (eqString.includes('-')) {
+      variables = eqString.split('-');
+      result = parseInt(variables[0]);
+      for (var i = 1; i < variables.length; i++) {
+        result = eval(result - parseInt(variables[i]))
+        console.log(result);
+      }
+      console.log(eqString + ' = ' + result);
+    }
+    $('#calc-disp').val(result);
+    equation = [result];
+    this.updatePaperTape(eqString ,result);
   }
 };
 $(document).ready(function() {
   calc.init();
   calc.updateCalcDisplay();
-  calc.updatePaperTape("3 / 4", "0.75");
-  calc.updatePaperTape("2 + 2", "4");
-  calc.updatePaperTape("5 * 5", "25");
-  calc.updatePaperTape("3 / 4", "0.75");
-  calc.updatePaperTape("2 + 2", "4");
-  calc.updatePaperTape("5 * 5", "25");
-  calc.updatePaperTape("3 / 4", "0.75");
-  calc.updatePaperTape("2 + 2", "4");
-  calc.updatePaperTape("5 * 5", "25");
-  calc.updatePaperTape("3 / 4", "0.75");
-  calc.updatePaperTape("2 + 2", "4");
-  calc.updatePaperTape("5 * 5", "25");
+  // calc.updatePaperTape("102938475609876543211234567890", "0.75");
+  // calc.updatePaperTape("2 + 2", "4");
+  // calc.updatePaperTape("5 * 5", "25");
+  // calc.updatePaperTape("3 / 4", "0.75");
+  // calc.updatePaperTape("2 + 2", "4");
+  // calc.updatePaperTape("5 * 5", "25");
+  // calc.updatePaperTape("3 / 4", "0.75");
+  // calc.updatePaperTape("2 + 2", "4");
+  // calc.updatePaperTape("5 * 5", "25");
+  // calc.updatePaperTape("3 / 4", "0.75");
+  // calc.updatePaperTape("2 + 2", "4");
+  // calc.updatePaperTape("5 * 5", "25");
 });
