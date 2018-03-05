a7e999b496f6dcf2f5e79be9f1e1f7f085f6337f
Cliff Minks <cliff.minks@gmail.com>
2018-03-04 21:41:24 -0600

    Add basic addition and subtraction specs without integration or replacement of master.js

---
+++ js/MathFns.js
@@ -0,0 +1,46 @@
+function MathFns() {
+}
+MathFns.prototype.add = function() {
+  var total = 0;
+  var args = Array.prototype.slice.call(arguments);
+  for (var i = 0; i < args.length; i++) {
+    total = total + args[i];
+  }
+  return total;
+};
+
+MathFns.prototype.subtract = function() {
+
+  var args = Array.prototype.slice.call(arguments);
+  var total = 0;
+  for (var i = 0; i < args.length; i++) {
+    if (i === 0) {
+      total = args[0];
+    }else {
+      total = total - args[i];
+    }
+  }
+  return total;
+};
+// Player.prototype.play = function(song) {
+//   this.currentlyPlayingSong = song;
+//   this.isPlaying = true;
+// };
+//
+// Player.prototype.pause = function() {
+//   this.isPlaying = false;
+// };
+//
+// Player.prototype.resume = function() {
+//   if (this.isPlaying) {
+//     throw new Error("song is already playing");
+//   }
+//
+//   this.isPlaying = true;
+// };
+//
+// Player.prototype.makeFavorite = function() {
+//   this.currentlyPlayingSong.persistFavoriteStatus(true);
+// };
+
+module.exports = MathFns;
---
+++ spec/math/MathFnsSpec.js
@@ -0,0 +1,67 @@
+describe("Calculator", function() {
+  var MathFns = require('../../js/MathFns');
+  var mfns;
+  beforeEach(function() {
+    mfns = new MathFns();
+  });
+
+  describe("Subtract", function() {
+    it('Should subtract 0 and 0', function() {
+      expect(mfns.subtract(0,0)).toEqual(0);
+    });
+    it("Should subtract two positive numbers", function() {
+      expect(mfns.subtract(1,2)).toEqual(-1);
+    });
+    it('Should subtract two positive numbers', function() {
+      expect(mfns.subtract(2,2)).toEqual(0);
+    });
+    it('Should subtract two positive numbers that equal a negative', function() {
+      expect(mfns.subtract(2,6)).toEqual(-4);
+    });
+    it('Should subtract two negative numbers', function() {
+      expect(mfns.subtract(-2,-6)).toEqual(4);
+    });
+    it("Should subtract more than two numbers", function() {
+      expect(mfns.subtract(1,2,3)).toEqual(-4);
+    });
+    it('Should subtract one negative and one positive number', function() {
+      expect(mfns.subtract(-2,6)).toEqual(-8);
+    });
+    it('Should subtract two Decimals', function() {
+      expect(mfns.subtract(1.5,0.5)).toEqual(1);
+    });
+    it('Should subtract a negative and a positive decimal', function() {
+      expect(mfns.subtract(-1.5,0.5)).toEqual(-2);
+    });
+  });
+
+  describe("Add", function() {
+    it("Should add two numbers", function() {
+      expect(mfns.add(1,2)).toEqual(3);
+    });
+    it("Should add more than two numbers", function() {
+      expect(mfns.add(1,2,3)).toEqual(6);
+    });
+	  it('Should add 0 and 0', function() {
+		  expect(mfns.add(0,0)).toEqual(0);
+	  });
+    it('Should add 2 and 2', function() {
+		  expect(mfns.add(2,2)).toEqual(4);
+	  });
+	  it('Should add positive numbers', function() {
+		  expect(mfns.add(2,6)).toEqual(8);
+	  });
+    it('Should add negative numbers', function() {
+      expect(mfns.add(-2,-6)).toEqual(-8);
+    });
+    it('Should add one negative and one positive number', function() {
+      expect(mfns.add(-2,6)).toEqual(4);
+    });
+    it('Should add two Decimals', function() {
+      expect(mfns.add(1.5,0.5)).toEqual(2);
+    });
+    it('Should add a negative and a positive decimal', function() {
+      expect(mfns.add(-1.5,0.5)).toEqual(-1);
+    });
+  });
+});
