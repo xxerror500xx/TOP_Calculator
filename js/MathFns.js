79033e23cae79f0ab40d17d47e91bbe72ea7dd15
Cliff Minks <cliff.minks@gmail.com>
2018-03-06 09:55:50 -0600

    Refactor basic math functions for clarity

--- js/MathFns.js
+++ js/MathFns.js
@@ -1,53 +1,49 @@
 function MathFns() {
 }
-MathFns.prototype.add = function(array) {
+MathFns.prototype.add = function(additives) {
   var total = 0;
-  var args = array //Array.prototype.slice.call(arguments);
-  for (var i = 0; i < args.length; i++) {
-    total = total + args[i];
+  for (var i = 0; i < additives.length; i++) {
+    total = total + additives[i];
   }
   return total;
 };
 
-MathFns.prototype.subtract = function(array) {
-  var args = array; //Array.prototype.slice.call(arguments);
+MathFns.prototype.subtract = function(subtractors) {
   var total = 0;
-  for (var i = 0; i < args.length; i++) {
+  for (var i = 0; i < subtractors.length; i++) {
     if (i === 0) {
-      total = args[0];
+      total = subtractors[0];
     }else {
-      total = total - args[i];
+      total = total - subtractors[i];
     }
   }
   return total;
 };
 
-MathFns.prototype.multiply = function(array) {
-  var args = array; // Array.prototype.slice.call(arguments);
+MathFns.prototype.multiply = function(multipliers) {
   var total = 0;
-  for (var i = 0; i < args.length; i++) {
-    if (args[i] === 0) {
+  for (var i = 0; i < multipliers.length; i++) {
+    if (multipliers[i] === 0) {
       return 0;
-    }else if (i === 0 && args[0] !== 0) {
-      total = args[0];
+    }else if (i === 0 && multipliers[0] !== 0) {
+      total = multipliers[0];
     }else {
-      total = total * args[i];
+      total = total * multipliers[i];
     }
   }
   return total;
 };
 
-MathFns.prototype.divide = function(array) {
-  var args = array; // Array.prototype.slice.call(arguments);
+MathFns.prototype.divide = function(divisors) {
   var total = 0;
-  for (var i = 0; i < args.length; i++) {
-    if (args[i] === 0) {
+  for (var i = 0; i < divisors.length; i++) {
+    if (divisors[i] === 0) {
       // console.log("You should not try to divide by zero.");
       return 0;
-    }else if (i === 0 && args[0] !== 0) {
-      total = args[0];
+    }else if (i === 0 && divisors[0] !== 0) {
+      total = divisors[0];
     }else {
-      total = total / args[i];
+      total = total / divisors[i];
     }
   }
   return total;
