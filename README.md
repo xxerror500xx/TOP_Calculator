bf8b5727a5128de2559211bac257ef53bf655f9e
Cliff Minks <cliff.minks@gmail.com>
2018-03-07 10:15:28 -0600

    Add Dev section to readme.

--- README.md
+++ README.md
@@ -4,6 +4,11 @@ Another assignment for the Odin Project using Js / Jquery, css, and html.
 - This application makes use of the [bootstrap Framework v4.0.0-beta.2](https://en.wikipedia.org/wiki/Bootstrap_(front-end_framework))
 - Jquery ver. 3.2.1
 
+DEV
+- NodeJS
+- NPM
+- Jasmine
+
 I did borrow some code and modified the [css for the buttons](https://codepen.io/tmrDevelops/pen/VeRvKX) from the amazing [Tiffany Rayside](https://codepen.io/tmrDevelops). OMG, so much talent in this one!
 
 This lesson can be found @ The Odin Project [Curriculum](https://www.theodinproject.com/courses/web-development-101/lessons/calculator)
--- js/Display.js
+++ js/Display.js
@@ -8,7 +8,7 @@ var display = {
       $('#solar-panel').addClass('solar-grid-clear-y');
       $('#solar-panel').removeClass('solar-grid-blue-y');
       $('#on-off-txt').text('ON');
-      this.clrAll();
+      calc.clrAll();
     } else {
       // remove sketch-btn-outline class
       $('#on-off-txt').text('OFF');
@@ -34,10 +34,6 @@ var display = {
       $('#' + buttons[i].id).removeClass(' btn sketch-btn ' + buttons[i].class);
       $('#' + buttons[i].id).addClass('sketch-btn-outline');
     }
-  },
-  updatePaperTape: function(equationString, result) {
-    // Row should equal pastEquations index
-    $('#p-tBody').append('<tr class=""><td class="tape-font">' + equationString + '</td><td class="tape-font"> = ' + result + '</td></tr>');
   },
   updateCalcDisplay: function() {
     if (equation.length === 1 && equation[0] !== '0') {
