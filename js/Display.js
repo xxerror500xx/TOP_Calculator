1e9fb286c017fb6fc623f8876a2f4de6bf1e9d42
Cliff Minks <cliff.minks@gmail.com>
2018-03-06 22:38:29 -0600

    Move Display functionality out of master in preperation for math and equation replacement

---
+++ js/Display.js
@@ -0,0 +1,55 @@
+var display = {
+  pwr: function() {
+    if (power) {
+      $('#on-off').removeClass('btn sketch-btn btn-danger');
+      $('#on-off').addClass('sketch-btn-outline');
+      $('#press-txt').removeClass('invisible');
+      this.rmvBtnClasses();
+      $('#solar-panel').addClass('solar-grid-clear-y');
+      $('#solar-panel').removeClass('solar-grid-blue-y');
+      $('#on-off-txt').text('ON');
+      this.clrAll();
+    } else {
+      // remove sketch-btn-outline class
+      $('#on-off-txt').text('OFF');
+
+      $('#on-off').removeClass('sketch-btn-outline');
+      $('#on-off').addClass('btn sketch-btn btn-danger');
+      $('#press-txt').addClass('invisible');
+      $('#solar-panel').addClass('solar-grid-blue-y');
+      $('#solar-panel').removeClass('solar-grid-clear-y');
+      this.addBtnClasses();
+    }
+    power = !power;
+    this.updateCalcDisplay();
+  },
+  addBtnClasses: function() {
+    for (var i = 0; i < buttons.length; i++) {
+      $('#' + buttons[i].id).removeClass('sketch-btn-outline');
+      $('#' + buttons[i].id).addClass('btn sketch-btn ' + buttons[i].class);
+    }
+  },
+  rmvBtnClasses: function() {
+    for (var i = 0; i < buttons.length; i++) {
+      $('#' + buttons[i].id).removeClass(' btn sketch-btn ' + buttons[i].class);
+      $('#' + buttons[i].id).addClass('sketch-btn-outline');
+    }
+  },
+  updatePaperTape: function(equationString, result) {
+    // Row should equal pastEquations index
+    $('#p-tBody').append('<tr class=""><td class="tape-font">' + equationString + '</td><td class="tape-font"> = ' + result + '</td></tr>');
+  },
+  updateCalcDisplay: function() {
+    if (equation.length === 1 && equation[0] !== '0') {
+      $('#calc-disp').val('');
+    }
+    if (equation.length > 0) {
+      $('#calc-disp').val($('#calc-disp').val() + equation[equation.length - 1]);
+    } else if (equation.length === 0) {
+      $('#calc-disp').val('0');
+    }
+    if (!power) {
+      $('#calc-disp').val('');
+    }
+  }
+}
--- js/master.js
+++ js/master.js
@@ -1,40 +1,4 @@
 var calc = {
-  pwr: function() {
-    if (power) {
-      $('#on-off').removeClass('btn sketch-btn btn-danger');
-      $('#on-off').addClass('sketch-btn-outline');
-      $('#press-txt').removeClass('invisible');
-      this.rmvBtnClasses();
-      $('#solar-panel').addClass('solar-grid-clear-y');
-      $('#solar-panel').removeClass('solar-grid-blue-y');
-      $('#on-off-txt').text('ON');
-      this.clrAll();
-    } else {
-      // remove sketch-btn-outline class
-      $('#on-off-txt').text('OFF');
-
-      $('#on-off').removeClass('sketch-btn-outline');
-      $('#on-off').addClass('btn sketch-btn btn-danger');
-      $('#press-txt').addClass('invisible');
-      $('#solar-panel').addClass('solar-grid-blue-y');
-      $('#solar-panel').removeClass('solar-grid-clear-y');
-      this.addBtnClasses();
-    }
-    power = !power;
-    calc.updateCalcDisplay();
-  },
-  addBtnClasses: function() {
-    for (var i = 0; i < buttons.length; i++) {
-      $('#' + buttons[i].id).removeClass('sketch-btn-outline');
-      $('#' + buttons[i].id).addClass('btn sketch-btn ' + buttons[i].class);
-    }
-  },
-  rmvBtnClasses: function() {
-    for (var i = 0; i < buttons.length; i++) {
-      $('#' + buttons[i].id).removeClass(' btn sketch-btn ' + buttons[i].class);
-      $('#' + buttons[i].id).addClass('sketch-btn-outline');
-    }
-  },
   addToEquation: function(value) {
     if (power) {
       if (equation.length === 0 && prevResult === "") {
@@ -42,24 +6,24 @@ var calc = {
           this.error('Cant start an equation with /,*, or +');
         }else if (value === '.') {
           equation.push('0' + value);
-          this.updateCalcDisplay();
+          display.updateCalcDisplay();
           prevResult = '';
         } else {
           equation.push(value);
-          this.updateCalcDisplay();
+          display.updateCalcDisplay();
           prevResult = '';
         }
       } else if (equation.length === 0 && prevResult !== "") {
         equation.push(prevResult);
         equation.push(value);
-        this.updateCalcDisplay();
+        display.updateCalcDisplay();
         prevResult = '';
       } else {
         if (prevResult !== "" && value.match(/(\D)/)) {
           equation.push(prevResult);
           equation.push(value);
           prevResult = '';
-          this.updateCalcDisplay();
+          display.updateCalcDisplay();
         } else if (equation[equation.length - 1].match(/\*|\/|\+/) && value.match(/\*|\/|\+/)) {
           this.error('To many math symbols in a row.');
         } else if (equation[equation.length - 1] === '-' && value === '-') {
@@ -67,34 +31,17 @@ var calc = {
           this.error('Don\'t be so negative :(');
         } else {
           equation.push(value);
-          this.updateCalcDisplay();
+          display.updateCalcDisplay();
           prevResult = '';
         }
       }
     }
   },
-  updateCalcDisplay: function() {
-    if (equation.length === 1 && equation[0] !== '0') {
-      $('#calc-disp').val('');
-    }
-    if (equation.length > 0) {
-      $('#calc-disp').val($('#calc-disp').val() + equation[equation.length - 1]);
-    } else if (equation.length === 0) {
-      $('#calc-disp').val('0');
-    }
-    if (!power) {
-      $('#calc-disp').val('');
-    }
-  },
-  updatePaperTape: function(equationString, result) {
-    // Row should equal pastEquations index
-    $('#p-tBody').append('<tr class=""><td class="tape-font">' + equationString + '</td><td class="tape-font"> = ' + result + '</td></tr>');
-  },
   clrCurrentEquation: function() {
     if (power) {
       equation = [];
       prevResult = '';
-      this.updateCalcDisplay();
+      display.updateCalcDisplay();
     }
   },
   clrAll: function() {
@@ -111,7 +58,7 @@ var calc = {
         equation.pop();
       } else if (equation.length === 0 && prevResult !== '') {
         prevResult = '';
-        this.updateCalcDisplay();
+        display.updateCalcDisplay();
       }
     }
   },
@@ -157,7 +104,7 @@ var calc = {
       }
       if (result === '¯＼(º_o)/¯') {
         $('#calc-disp').val(result);
-        this.updatePaperTape(eqString, result);
+        display.updatePaperTape(eqString, result);
         prevResult = '';
         equation = [];
         result = '';
@@ -166,12 +113,12 @@ var calc = {
         prevResult = result;
         equation = [];
         result = '';
-        this.updatePaperTape(eqString, prevResult);
+        display.updatePaperTape(eqString, prevResult);
       }
     }
   }
 };
 $(document).ready(function() {
   kbd.init();
-  calc.updateCalcDisplay();
+  display.updateCalcDisplay();
 });
