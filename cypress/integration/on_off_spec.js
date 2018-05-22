5e9127f89f65fe422966af87ebd6673f0f34b4eb
xxerror500xx <cliff.minks@gmail.com>
2018-05-22 17:52:08 -0500

    Refactor on off integration spec

--- cypress/integration/on_off_spec.js
+++ cypress/integration/on_off_spec.js
@@ -1,23 +1,34 @@
 describe('Calculator On-Off', function() {
-  it('Loads grey and white buttons', function() {
+  beforeEach(function(){
     cy.visit('/')
-    cy.get('#on-off')
-      .should('not.have.class', 'btn-danger')
-      .click()
-    cy.get('#on-off')
-      .should('have.class', 'btn-danger')
-      .click()
   })
-  it('Notifies to press on button', function() {
-    cy.get('#press-txt')
-      .should('not.have.class', 'invisible')
-    cy.get('#on-off')
-      .should('not.have.class', 'btn-danger')
-      .click()
-    cy.get('#press-txt')
-      .should('have.class', 'invisible')
-    cy.get('#on-off')
-      .should('have.class', 'btn-danger')
-      .click()
+  context('Before pressing on', function() {
+    it('Loads grey and white buttons', function() {
+      cy.visit('/')
+      cy.get('#on-off')
+        .should('not.have.class', 'btn-danger')
+    })
+    it('Label to press on button is visible', function() {
+      cy.get('#press-txt')
+        .should('not.have.class', 'invisible')
+      cy.get('#on-off')
+        .should('not.have.class', 'btn-danger')
+    })
+  })
+
+  context('After pressing the on button', function() {
+    beforeEach(function() {
+      cy.visit('/')
+      cy.get('#on-off')
+        .click()
+    })
+    it('Loads grey and white buttons', function() {
+      cy.get('#on-off')
+        .should('have.class', 'btn-danger')
+    })
+    it('Lable to turn on is hidden', function() {
+      cy.get('#press-txt')
+        .should('have.class', 'invisible')
+    })
   })
 })
